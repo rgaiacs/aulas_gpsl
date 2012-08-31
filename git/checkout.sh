30aaf878e11db476a840be3a6d92c7b79aff4fe3
Raniere Silva <raniere@sr150-ubuntu.(none)>
2012-08-30 21:59:50 -0300

    Inicio de tutorial sobre git.

---
+++ git/checkout.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright (C) 2012  Raniere Silva
+#
+# This file is part of 'Aprendendo git com bash scripts'.
+#
+# 'Aprendendo git com bash scripts' is free software: you can
+# redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# 'Aprendendo git com bash scripts' is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Utiliza init.sh
+./init.sh > /dev/null
+cd temp
+
+# Retorna a versao anterior.
+cat texto.txt
+git checkout HEAD^
+cat texto.txt
+
+# Retorna para a ultima versao salva.
+git checkout master
+cat texto.txt
